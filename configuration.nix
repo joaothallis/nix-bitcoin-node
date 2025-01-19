a817b7d43e7f3a683c50d6895dfe8db7de503cf4
João Thallis <joaothallis@icloud.com>
2025-01-19 12:58:15 -0300

    feat: Disable LND and RTL
    This commit disables LND and Ride the Lightning (RTL)

--- configuration.nix
+++ configuration.nix
@@ -14,13 +14,6 @@
 
   services.bitcoind.txindex = true;
 
-  services.lnd.enable = true;
-  nix-bitcoin.onionServices.lnd.public = true;
-
-  services.rtl.enable = true;
-  services.rtl.nodes.lnd.enable = true;
-  services.rtl.nodes.lnd.loop = true;
-
   services.mempool.enable = true;
 
   nix-bitcoin.onionServices.mempool-frontend.enable = true;
