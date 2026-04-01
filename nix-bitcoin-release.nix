c0bc08787315ff3b830bb7ebce74955888b89895
João Thallis <joaothallis.developer@gmail.com>
2026-03-31 22:44:29 -0300

    update-nix-bitcoin

--- nix-bitcoin-release.nix
+++ nix-bitcoin-release.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/fort-nix/nix-bitcoin/archive/v0.0.117.tar.gz";
-  sha256 = "sha256-JN/PFBOVqWKc76zSdOunYoG5Q0m8W4zfrEh3V4EOIuk=";
+  url = "https://github.com/fort-nix/nix-bitcoin/archive/v0.0.135.tar.gz";
+  sha256 = "sha256-UzRxDiJlopBGPTjyhCdMP+QdTwXK+l+y45urXCyH69A=";
 }
